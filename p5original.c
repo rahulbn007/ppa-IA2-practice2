7acec5dd3b722c5f66615c35147913ea5dc1b11b
rahulbn007 <96820417+rahulbn007@users.noreply.github.com>
2022-02-09 07:15:06 +0000

    .

--- p5original.c
+++ p5original.c
@@ -1,12 +1,29 @@
-#incude<stdio.h>
+#include<stdio.h>
 int input()
 {
   int a;
-  printf("Enter a number:\n");
-  scanf("%d",a);
+  printf("Enter a positive number:\n");
+  scanf("%d",&a);
   return a;
 }
 int gcd(int a,int b)
 {
-  
+  int i,gcd;
+  for(i=1; i<=a && i<=b; ++i)
+    if(a%i==0 && b%i==0)
+      gcd=i;
+  return gcd;
+}
+void output(int a, int b, int gcd)
+{
+  printf("GCD of %d and %d is %d",a,b,gcd);
+}
+int main()
+{
+  int x,y,z;
+  x=input();
+  y=input();
+  z=gcd(x,y);
+  output(x,y,z);
+  return 0;
 }
