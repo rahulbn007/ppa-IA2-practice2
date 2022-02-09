bcd385a59d4fbe8b65ec9c779dd4b50ffea44ad4
rahulbn007 <96820417+rahulbn007@users.noreply.github.com>
2022-02-09 05:42:18 +0000

    .

--- p3original.c
+++ p3original.c
@@ -8,5 +8,26 @@ int input_number()
 }
 int is_composite(int n)
 {
-  
+  int c=0,i;
+  for(i=1;i<=n;i++)
+    {
+      if(n % i == 0)
+        c++;
+    }
+  return c;
+}
+void output(int n,int composite)
+{
+  if(composite>2)
+   printf("%d is a composite number.",n);
+  else
+    printf("it is not a composite number.");
+}
+int main()
+{
+  int a,b;
+  a=input_number();
+  b=is_composite(a);
+  output(a,b);
+  return 0;
 }
---
+++ p4original.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int input_array_size()
+{
+  int n;
+  printf("Enter the size of array:\n");
+  scanf("%d",&n);
+  return n;
+}
